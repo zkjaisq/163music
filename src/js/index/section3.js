0e3109d8055fe5eda375841c12315da3cb257b42
zkjaisq <18771964684@163.com>
2018-03-06 11:07:36 +0800

    添加了搜索框

--- src/js/index/section3.js
+++ src/js/index/section3.js
@@ -1,33 +1,50 @@
 {
-    let view ={
-        el:'.page-3',
-        init(){
-            this.$el =$(this.el)
+    let view = {
+        el: '.page-3',
+        init() {
+            this.$el = $(this.el)
         },
-        show(){
+        show() {
             this.$el.addClass('active')
         },
-        hide(){
+        hide() {
             this.$el.removeClass('active')
+        },
+        render() {
+
         }
     }
-    let model={}
-    let controller ={
-        init(){
-            this.view =view
+    let model = {
+        data: {
+            song: []
+        }
+    }
+    let controller = {
+        init() {
+            this.view = view
             this.model = model
             this.view.init()
             this.bindEventHub()
         },
-        bindEventHub(){
-            window.eventHub.on('selectTab',(pageName)=>{
-                if(pageName === 'page-3'){
+        bindEventHub() {
+            window.eventHub.on('selectTab', (pageName) => {
+                if (pageName === 'page-3') {
                     this.view.show()
-                }else{
+                } else {
                     this.view.hide()
                 }
             })
+          this.view.$el.find('input').on('input',(e)=>{
+            this.view.$el.find('.delete').addClass('active')  
+            let $input = $(e.currentTarget)
+              let value =$input.val()
+              let  song = new AV.Query('Song');
+              song.contains('content',value);
+              song.find().then((result)=>{
+              console.log(result)
+              })
+          })
         }
     }
-    controller.init(view,model)
+    controller.init(view, model)
 }
